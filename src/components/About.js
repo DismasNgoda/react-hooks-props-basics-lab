e7f0be0755ef7818ec7ad2479ca15326090a753d
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-09-24 17:05:58 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,16 @@ import Links from "./Links";
 
 function About({bio, links}) {
   function bioData(){
-    if (bio && bio.length > 1){
-      return <p>{bio}</p>;
-    } else {
+    if (!bio || bio ===""){
       return null;
+    } else {
+      return <p>{bio}</p>;
     }
   }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{bioData()}</p>    
+      {bioData()}   
      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
      <Links github={links.github} linkedin={links.linkedin} />
